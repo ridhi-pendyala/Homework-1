75c7efbfa4b9f176554ee0b2fbd64d010b590d78
ridhulu <ridhi.pendyala@gmail.com>
2026-01-21 23:31:01 -0500

    test

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        String SONNET = """ 
+                Two households, both alike in dignity,
+                In fair Verona, where we lay our scene,
+                From ancient grudge break to new mutiny,
+                Where civil blood makes civil hands unclean.
+                From forth the fatal loins of these two foes
+                A pair of star-cross’d lovers take their life;
+                Whose misadventured piteous overthrows
+                Do with their death bury their parents’ strife.
+                The fearful passage of their death-mark’d love,
+                And the continuance of their parents’ rage,
+                Which, but their children’s end, nought could remove,
+                Is now the two hours’ traffic of our stage;
+                The which if you with patient ears attend,
+                What here shall miss, our toil shall strive to mend.""";
+
+            int correct = 0;
+            int incorrect = 0;
+            Scanner scanner = new Scanner(System.in);
+            Random random = new Random();
+
+            //sonnet broken up by spaces, but keeping punctuation attached to the closest word
+
+            String[] words = SONNET.split("\\s+");
+
+
+
+            while (correct < 3 && incorrect < 3) {
+
+                int stop = random.nextInt(words.length);
+
+               //removes the punctuation, but keeps apostrophe and -, which are used in the words
+                String correctWord = words[stop].replaceAll("[\\p{Punct}&&[^’'-]]", "");
+
+                for (int i = 0; i < stop; i++) {
+                    System.out.print(words[i] + " ");
+                }
+                System.out.println("_____");
+
+                System.out.print("What word comes next? ");
+                String input = scanner.nextLine();
+
+                if (input.equalsIgnoreCase(correctWord)) {
+                    System.out.println("That was correct!\n");
+                    correct++;
+                } else {
+                    System.out.println("That was incorrect. The correct word was: " + correctWord + "\n");
+                    incorrect++;
+                }
+
+            }
+            scanner.close();
+
+
+    }
+}
